1902499a274c262b158f8436e415291005404524
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-12-21 22:54:52 +0100

    fix(test): update the loading condition of the info

--- src/modules/common/LoadingComponent.tsx
+++ src/modules/common/LoadingComponent.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import { Skeleton } from '@mui/material';
 
 export const LoadingComponent = ({
   testId,
   isLoading,
   children,
+  firstLoadOnly = false,
 }: {
   testId?: string;
   isLoading: boolean;
   children: React.ReactNode;
-}): React.ReactNode =>
-  isLoading ? <Skeleton data-testid={testId}>{children}</Skeleton> : children;
+  firstLoadOnly?: boolean;
+}): React.ReactNode => {
+  // count number of isLoading to show skeleton on first load if needed.
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (isLoading) {
+      setCounter((c) => c + 1);
+    }
+  }, [isLoading]);
+
+  if (!isLoading || (counter > 1 && firstLoadOnly)) {
+    return children;
+  }
+
+  return <Skeleton data-testid={testId}>{children}</Skeleton>;
+};
